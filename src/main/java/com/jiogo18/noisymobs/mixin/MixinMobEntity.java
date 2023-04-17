df7ef4097f4d2bf3a8dcbaccec82722e7e4a4729
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2023-04-17 21:55:23 +0200

    "Discussion" distribution & neighbour factor

--- src/main/java/com/jiogo18/noisymobs/mixin/MixinMobEntity.java
+++ src/main/java/com/jiogo18/noisymobs/mixin/MixinMobEntity.java
@@ -6,12 +6,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.jiogo18.noisymobs.engine.SoundLimiterEngine;
+import com.jiogo18.noisymobs.engine.SoundDistribution;
+import com.jiogo18.noisymobs.engine.SoundPlayer;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
@@ -21,43 +21,38 @@ public abstract class MixinMobEntity extends LivingEntity {
 		super(type, world);
 	}
 
-	private void resetAmbientSoundTime() {
-		MobEntity mob = (MobEntity) (Object) this;
-
-		float r1 = this.random.nextFloat();
-		float r2 = this.random.nextFloat();
-
-		// Longer minimun interval between sounds
-		mob.ambientSoundTime = (int) (-mob.getAmbientSoundInterval() * r1 * r2 * 2);
-	}
-
 	@Shadow
 	protected abstract SoundEvent getAmbientSound();
 
-	@Override
-	public void baseTick() {
-		super.baseTick();
-		MobEntity mob = (MobEntity) (Object) this;
-		mob.level.getProfiler().push("mobBaseTick");
-		if (mob.isAlive()) {
-			mob.ambientSoundTime++;
-			if (mob.ambientSoundTime >= 0) {
-				if (this.random.nextInt(200) < 1) { // ~ every 10 seconds
-					this.resetAmbientSoundTime();
-					mob.playAmbientSound();
-				}
-			}
-		}
-		mob.level.getProfiler().pop();
+	@Shadow
+	public int ambientSoundTime;
+
+	@Shadow
+	public abstract void playAmbientSound();
+
+	private void resetCustomAmbientSoundTime() {
+		this.ambientSoundTime = -SoundDistribution.getNextAmbientSoundTicks((MobEntity) (Object) this);
 	}
 
-	public boolean isInteractingWithPlayer() {
-		MobEntity mob = (MobEntity) (Object) this;
-		if (mob instanceof AbstractVillagerEntity) {
-			AbstractVillagerEntity villager = (AbstractVillagerEntity) mob;
-			return villager.isTrading();
+	@Inject(method = "resetAmbientSoundTime", at = @At("HEAD"), cancellable = true)
+	protected void resetAmbientSoundTime(CallbackInfo info) {
+		info.cancel();
+		resetCustomAmbientSoundTime();
+	}
+
+	@Inject(method = "baseTick", at = @At("HEAD"), cancellable = true)
+	public void baseTick(CallbackInfo info) {
+		super.baseTick();
+		this.level.getProfiler().push("mobBaseTick");
+		if (this.isAlive()) {
+			this.ambientSoundTime++;
+			if (this.ambientSoundTime >= 0) {
+				resetCustomAmbientSoundTime();
+				this.playAmbientSound();
+			}
 		}
-		return false;
+		this.level.getProfiler().pop();
+		info.cancel();
 	}
 
 	@Inject(method = "playAmbientSound", at = @At("HEAD"), cancellable = true)
@@ -65,12 +60,9 @@ public abstract class MixinMobEntity extends LivingEntity {
 		info.cancel();
 		SoundEvent soundevent = this.getAmbientSound();
 		if (soundevent != null) {
-			if (isInteractingWithPlayer() ||
-					SoundLimiterEngine.getAmbientSoundLimiter().canPlayNewSound(this.position())) {
-				// Can speak quieter than normal
-				float volume = Math.max(0, this.getSoundVolume() - this.random.nextFloat());
-				this.playSound(soundevent, volume, this.getVoicePitch());
-			}
+			// Can speak quieter than normal
+			float volume = Math.max(0, this.getSoundVolume() - this.random.nextFloat());
+			SoundPlayer.playAmbientSound((MobEntity) (Object) this, soundevent, volume, this.getVoicePitch());
 		}
 	}
 }
